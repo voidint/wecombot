d973e35dd88a08107959defebb84d9b983061cc0
voidint <voidint@126.com>
2023-09-02 14:59:23 +0800

    refactor: 变更图文消息属性数据类型

--- news.go
+++ news.go
@@ -12,10 +12,14 @@ type NewsMessage struct {
 
 // Article 图文
 type Article struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	URL         string `json:"url"`
-	PicURL      string `json:"picurl"`
+	// Title 标题，不超过128个字节，超过会自动截断。
+	Title string `json:"title"`
+	// Description 描述，不超过512个字节，超过会自动截断。
+	Description *string `json:"description"`
+	// URL 点击后跳转的链接。
+	URL string `json:"url"`
+	// PicURL 图文消息的图片链接，支持JPG、PNG格式，较好的效果为大图 1068*455，小图150*150。
+	PicURL *string `json:"picurl"`
 }
 
 // SendNewsMessage 发送图文消息
